7a8afb659559298dd6647c57ba1d6041e1474f29
SindhuKilari99 <108974193+SindhuKilari99@users.noreply.github.com>
2022-07-13 15:01:05 +0530

    Create PROGRAM-4.c

---
+++ PROGRAM-4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    long int rem,num,cnt=0;
+    printf("enter the lottery tickets:");
+    scanf("%ld",&num);
+    label:
+    if(num!=0)
+    {
+        rem=num%10;
+        if(rem==3)
+        cnt++;
+        num/=10;
+        goto label;
+    }
+    if(cnt>0)
+    printf("%ld",cnt);
+}
