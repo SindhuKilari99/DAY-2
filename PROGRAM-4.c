a7c437a5d149eef184506622b1c0cab3f77f7834
SindhuKilari99 <108974193+SindhuKilari99@users.noreply.github.com>
2022-07-13 15:05:23 +0530

    Update PROGRAM-4.c

--- PROGRAM-4.c
+++ PROGRAM-4.c
@@ -16,3 +16,26 @@ int main()
     if(cnt>0)
     printf("%ld",cnt);
 }
+
+
+
+
+
+#include <stdio.h>
+int main()
+{
+	
+	int a[]={1,3,4,5,6,3,3,6,3,3,3};
+
+	int n=sizeof(a)/sizeof(a[0]);
+	int i,count=0;
+	
+	for(i=0;i<n;i++)
+	{
+		if(a[i]==3)
+			
+			count++;
+	}
+	
+	printf("%d",count);
+}
